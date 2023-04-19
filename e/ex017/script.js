e34bb705ddb9a5352b60d1232d483fc6d6a90c85
João Félix <113360774+JMoreiraFelix@users.noreply.github.com>
2023-04-19 10:21:48 -0300

    update ex017

--- e/ex017/script.js
+++ e/ex017/script.js
@@ -1,14 +1,16 @@
 function gerar() {
     var numero = document.getElementById('txtn')
-    var n = Number(numero.value)
     if (numero.value.length == 0) {
         alert('[ERROR] INSIRA UM VALOR')
     } else {
-        resultado = document.getElementById('res')
-        resultado.innerHTML = ''
+        var tabuada = document.getElementById('tab')
+        tabuada.innerHTML = ''
+        var n = Number(numero.value)
     for (var contador = 1; contador <= 10; contador++) {
-        multiplicação = n * contador
-        resultado.innerHTML += `<p> ${n} x ${contador} = ${multiplicação} </p>`
+        var item = document.createElement('option')
+        item.text = `${n} x ${contador} = ${n * contador}`
+        item.value = `tabuada ${contador}`
+        tabuada.appendChild(item) 
         }
     }
 }
