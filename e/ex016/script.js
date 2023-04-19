2c056f0a40ad41fd890f6255591e214bde7d3664
João Félix <113360774+JMoreiraFelix@users.noreply.github.com>
2023-04-19 09:06:22 -0300

    update exs

--- e/ex016/script.js
+++ e/ex016/script.js
@@ -1,13 +1,30 @@
-c = 1
-while (c <=3) {
-    console.log("Olá")
-    c++
+function contar() {
+    var inicio = document.getElementById('txti')
+    var i = Number(inicio.value)
+    var fim = document.getElementById('txtf')
+    var f = Number(fim.value)
+    var passo = document.getElementById('txtp')
+    var p = Number(passo.value)
+    var resultado = document.getElementById('res')
+    
+    var soma = i + p
+    if (inicio.value.length == '' || fim.value.length == '' || passo.value.length == '') {
+        alert("[ERROR]")
+        resultado.innerHTML = "Impossível de contar :("
+    } else if (p == 0) {
+        alert("[PASSO INVÁLIDO ! SERÁ CONSIDERADO PASSO = 1]")
+        p = 1
+        soma = i + p
+        resultado.innerHTML = `${i} `
+        while (soma <= f ) {
+            resultado.innerHTML += `/ ${soma} `
+            soma += p
+            }
+    } else {
+        resultado.innerHTML = `${i} `
+    while (soma <= f ) {
+        resultado.innerHTML += `/ ${soma} `
+        soma += p
+        }
+    }
 }
-
-// ou
-c = 1
-
-do {
-    console.log("Olá")
-    c++
-} while (c <=3)
--- e/ex017/script.js
+++ e/ex017/script.js
@@ -1,3 +1,14 @@
-for (var c = 1; c < 9; c++) {
-    console.log(c)
+function gerar() {
+    var numero = document.getElementById('txtn')
+    var n = Number(numero.value)
+    if (numero.value.length == 0) {
+        alert('[ERROR] INSIRA UM VALOR')
+    } else {
+        resultado = document.getElementById('res')
+        resultado.innerHTML = ''
+    for (var contador = 1; contador <= 10; contador++) {
+        multiplicação = n * contador
+        resultado.innerHTML += `<p> ${n} x ${contador} = ${multiplicação} </p>`
+        }
+    }
 }
