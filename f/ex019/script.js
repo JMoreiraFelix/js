a094590c1a657a0b49570211128bd8af8d8f3987
João Félix <113360774+JMoreiraFelix@users.noreply.github.com>
2023-04-20 11:32:00 -0300

    update ex 019

--- f/ex019/script.js
+++ f/ex019/script.js
@@ -1,3 +1,4 @@
+
 var numero = document.getElementById('txtn')
 var tabela = document.getElementById('tab')
 var resultado = document.getElementById('res')
@@ -21,8 +22,39 @@ function inTabela(n, t) {
 
 function adicionar() {
     if (isNumero(numero.value) && inTabela(numero.value, valores)) {
-        alert('OK')
+        valores.push(Number(numero.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${numero.value} adicionado.`
+        tabela.appendChild(item)
     } else {
         alert('[ERROR] NÚMERO INVÁLIDO')
+    }
+    numero.value = ''
+    numero.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('[ERROR] SEM VALORES')
+    } else {
+        var resultado = document.getElementById('res')
+        resultado.innerHTML = `<p>Ao todo, temos ${valores.length} números.<\p>`
+
+        var max = Math.max(...valores)
+        resultado.innerHTML += `<p>O maior valor informado foi ${max}.<\p>`
+
+        var min = Math.min(...valores)
+        resultado.innerHTML += `<p>O menor valor informado foi ${min}.<\p>`
+
+        var soma = 0
+        for (var c = 0; c < valores.length; c++) {
+            soma += valores[c]
+        }
+        resultado.innerHTML += `<p>A soma de todos os valores é ${soma}.<\p>`
+
+        var media = soma / valores.length
+        m = String(media)
+        media = m.replace("." , ",")
+        resultado.innerHTML += `<p>A média de todos os valores é ${media}.<\p>`
     }
 }
